33a73f0a2b43db39edbfc4159a2c460722af00f9
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-01 09:27:46 -0300

    test

--- trydjango/trydjango/urls.py
+++ trydjango/trydjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages import views
+
 urlpatterns = [
+    path('', views.home_views, name='home'),
     path('admin/', admin.site.urls),
 ]
