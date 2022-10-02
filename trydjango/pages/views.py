d73e01958b7b20e91bc93e200a5d606acf5210d9
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-02 19:02:45 -0300

    update

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -9,4 +10,14 @@ def home_view(request,*args, **kwargs):
     return render(request, "home.html", {})
 
 def contact_view(request,*arg, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
+
+def about_view(request,*arg, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "this_is_true" : True,
+        "my_number": 123,
+        "my_list" : [1313, 4231, 312, "Abc"],
+        "my_html" : "<h1>test<h1>",
+    }
+    return render(request, "about.html", my_context)
